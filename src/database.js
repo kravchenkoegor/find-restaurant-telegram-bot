38a083bb4ff33fda5b330acbc75050f1181e5253
ekravchenko <e.kravchenko@artsofte.ru>
2018-05-11 15:11:21 +0500

    develop

--- src/database.js
+++ src/database.js
@@ -9,10 +9,6 @@ mongoose.connect(`${process.env.PROD_MONGODB}`, mongooseOptions)
   .catch((err) => console.log(err))
 
 require('./model/ekb-food.model')
-const Food = mongoose.model('ekb-food')
+export const Food = mongoose.model('ekb-food')
 require('./model/user.model')
-const User = mongoose.model('usersEkb')
-
-export default {
-  Food, User
-}
+export const User = mongoose.model('usersEkb')
