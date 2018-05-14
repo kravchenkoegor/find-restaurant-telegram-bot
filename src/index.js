63835cb473682647d23f3e3b10e264e5a77a6166
Egor Kravchenko <kravchenko.egor@gmail.com>
2018-05-14 19:26:15 +0500

    devel home

--- src/index.js
+++ src/index.js
@@ -464,7 +464,9 @@ function calcDistance (location) {
     place.forEach(p => {
       p.distance = geolib.getDistance(location, p.location) / 1000
     })
-    return _.sortBy(place, 'distance').slice(0, itemsLimit * 3)
+    return {
+      place: _.sortBy(place, 'distance').slice(0, itemsLimit * 3)
+    }
     // place.map((p, idx) => {
     //   if (p.description) {
     //     return `<b>${idx + 1}. ${p.title}</b>\n<em>${p.description}</em>\n${p.address}\nРасстояние ${p.distance} км\n${p.uuid}`
@@ -473,7 +475,7 @@ function calcDistance (location) {
     //   }
     // }).join('\n')
     // return place
-  })
+  }).catch(err => console.log(err))
 }
 
 
